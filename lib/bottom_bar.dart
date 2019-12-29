f97567a97e2475cba5c95ee9fbfb7a246735a442
gislainy <gislainy.velasco@vigiadepreco.com.br>
2019-12-28 21:11:08 -0300

    Home page

---
+++ lib/bottom_bar.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        notchMargin: 6.0,
+        color: Colors.transparent,
+        elevation: 9.0,
+        clipBehavior: Clip.antiAlias,
+        child: Container(
+          height: 50.0,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25.0),
+                topRight: Radius.circular(25.0)),
+          ),
+          child: Row(
+            children: <Widget>[
+              Container(
+                height: 50.0,
+                width: MediaQuery.of(context).size.width / 2 - 40.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(Icons.home, color: Color(0xFFEF7532)),
+                    Icon(Icons.person_outline, color: Color(0Xff676e79)),
+                  ],
+                ),
+              ),
+              Container(
+                height: 50.0,
+                width: MediaQuery.of(context).size.width / 2 - 40.0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(Icons.search, color: Color(0xFFEF7532)),
+                    Icon(Icons.shopping_basket, color: Color(0Xff676e79)),
+                  ],
+                ),
+              )
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          ),
+        ));
+  }
+}
